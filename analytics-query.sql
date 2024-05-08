3d1710504d57499cbe48e964a6014bc4f833c307
Keshav Jha <keshav17082003@gmail.com>
2024-05-08 21:55:43 +0530

    first commit

---
+++ analytics-query.sql
@@ -0,0 +1,37 @@
+CREATE OR REPLACE TABLE `keshav-uber-analytics.keshav_final_uber.powerbi_analytics` AS (
+SELECT 
+    f.VendorID,
+    d.tpep_pickup_datetime,
+    d.tpep_dropoff_datetime,
+    p.passenger_count,
+    t.trip_distance,
+    r.rate_code_name,
+    pick.pickup_latitude,
+    pick.pickup_longitude,
+    drop.dropoff_latitude,
+    drop.dropoff_longitude,
+    pay.payment_type_name,
+    f.fare_amount,
+    f.extra,
+    f.mta_tax,
+    f.tip_amount,
+    f.tolls_amount,
+    f.improvement_surcharge,
+    f.total_amount
+FROM 
+    `keshav-uber-analytics.keshav_final_uber.fact_table` f
+JOIN 
+    `keshav-uber-analytics.keshav_final_uber.datetime_dim` d ON f.datetime_id=d.datetime_id
+JOIN 
+    `keshav-uber-analytics.keshav_final_uber.passenger_count_dim` p ON p.passenger_count_id=f.passenger_count_id  
+JOIN 
+    `keshav-uber-analytics.keshav_final_uber.trip_distance_dim` t ON t.trip_distance_id=f.trip_distance_id  
+JOIN 
+    `keshav-uber-analytics.keshav_final_uber.rate_code_dim` r ON r.rate_code_id=f.rate_code_id  
+JOIN 
+    `keshav-uber-analytics.keshav_final_uber.pickup_location_dim` pick ON pick.pickup_location_id=f.pickup_location_id
+JOIN 
+    `keshav-uber-analytics.keshav_final_uber.dropoff_location_dim` drop ON drop.dropoff_location_id=f.dropoff_location_id
+JOIN 
+    `keshav-uber-analytics.keshav_final_uber.payment_type_dim` pay ON pay.payment_type_id=f.payment_type_id
+);
